ff1275752220fd6a9ef594521b8d6af49bfb48a3
wispoz <wispoz@inbox.ru>
2014-08-15 19:00:13 +0400

    Db change f

--- app/src/main/java/com/wispoz/motivateme/MyDatabase.java
+++ app/src/main/java/com/wispoz/motivateme/MyDatabase.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteQueryBuilder;
 public class MyDatabase extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "motivates.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public MyDatabase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);

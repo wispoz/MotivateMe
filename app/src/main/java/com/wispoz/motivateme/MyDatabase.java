59ca3b8b6369898e6b45149858c485c04194ab44
wispoz <wispoz@mp-cd-ws-010.metropolis.russia>
2014-08-15 18:56:55 +0400

    Initial commit

---
+++ app/src/main/java/com/wispoz/motivateme/MyDatabase.java
@@ -0,0 +1,19 @@
+package com.wispoz.motivateme;
+
+import android.content.Context;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+/**
+ * Created by wispoz on 15.08.14.
+ */
+public class MyDatabase extends SQLiteAssetHelper {
+
+    private static final String DATABASE_NAME = "motivates.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MyDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+}
